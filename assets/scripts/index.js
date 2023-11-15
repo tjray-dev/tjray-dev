ad6d778ed1519c645101180331dbf0125b5a5b7c
Tate Ray <tate@tateray.com>
2023-11-15 12:43:46 -0700

    initial js framed up;

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,88 +1,86 @@
-let projects = 
-{
-    TacticsMaster: 
-    {
-        techStack: 
-        [
-            "godot", 
-            "C#",
-        ],
-        description: 
-        [
-            "Tactics Master is a proof of concept for a tactical rpg. ", 
-            "It was built using the Godot game engine and the C# programming language. ",
-            "It utilizes a square grid based tilemap along with an A Star search algorithm for pathfinding, ",
-            "this is done by utilizing a combination of basic data structures that allow for minimal memory usage. ",
-            "The game also features an auto battle system that makes intelligent use of the the maps terrain and opponent weaknesses. ",
-            "C# was chosen as the scripting language for this project to allow me to focus on learning the ins and outs of the engine while continuing to hone my C# skills. "
-        ],
-        highlight: 
-        {
-            text: "For this project I most proud of the final iteration of the A Star algorithm",
-            codeSample: "!!this should be a code sample!!"
-        },
-    },
-    ShadowsAndBonfires:
-    {
-        techStack: 
-        [
-            "godot",
-            "GDScript",
-            "C#"
-        ],
-        description: 
-        [
-            "Shadows and Bonfires is an adventure Role Playing Game inspired by the original Legend of Zelda. ",
-            "It utilizes a minimalist and abstract graphics form to help reinforce the themes of the game. ",
-            "Players journey through a gloomy and hostile world seeking to overcome foes, collect equipment, and defeat powerful bosses. ",
-            "This games makes strong use of text and color to convey not only aesthetic but also enemy type, strength, and weakness. ",
-            "The game also relies on little in the way of direct story-telling, through dialogue or other text-based narrative, ",
-            "instead choosing to allow mechanics and environment to express this with the intent being to allow the player to engage with the themes ",
-            "in a game world that responds to player action as well as their successes and failures."
-        ],
-        highlight: 
-        {
-            text: "I would like to highlight a few of the more explicit ways in which the game reinforces its themes to the player",
-            highlightImgs: "!!this should be a collection of screen shots",
-        },
-    },
-    SimplySudsSite:
-    {
-        techStack: 
-        [
-            "HTML",
-            "CSS",
-            "JavaScript",
-        ],
-        description: 
-        [
-            "This is a mock up e-commerce page for a boutique wellness company called Simply Suds & More.",
-            "I was hired to manage this companies online presence across multiple sales platforms, as well as design their various sites as applicable",
-            "This mock up was created as a prototype to test and finalize the overall designs for the company while allowing me to refine my skills with web based development. ",
+let json;
 
-        ],
-        highlight: "This site was built without the use of any JavaScript frameworks, relying only on vanilla JavaScript and extensive knowledge of the power of HTML and CSS. "
-    },
-    BBQCookingSchool:
-    {
-        techStack: 
-        [
-            "HTML",
-            "CSS",
-            "JavaScript",
-            "Vue",
-        ],
-        description:
-        [
-            "This site is a working webpage for a business that sells a correspondence course that provides certification as a Master BBQ cook, ",
-            "as well as counting towards credit hours for continued education in the culinary arts. ",
-            "The site utilizes the Vue JavaScript framework's CDN as a light weight and inline JavaScript solution while still making use of modern Single Page Application principles. ",
-            "This solution allows the site to act as a SPA while still showcasing the power of well written HTML and CSS. "
-        ],
-        highlight:
-        {
-            text: "For this project I would like to highlight a few examples of how I utilized the Vue CDN.",
-            codeSample: "!!this should be a code sample!!"
-        }
-    },
+async function fetchJson()
+{
+    const response = await fetch("./assets/JSON/Text.JSON")
+    const data = await response.json();
+    json = await data;
 }
+
+async function StartUp() 
+{
+    await fetchJson();
+    addEventListener("DOMContentLoaded", renderLanding());  
+}
+
+function renderLanding() 
+{
+    let blurbP = document.createElement("p");
+    let blurbDiv = document.createElement("div");
+    let heading = document.getElementsByClassName("heading")[0];
+    let subHeading = document.getElementsByClassName("sub")[0];
+    let text = document.getElementById("text");
+
+    heading.innerHTML = "";
+    subHeading.innerHTML = "";
+    text.innerHTML = "";
+
+    heading.innerHTML = "Welcome To BBQCookingSchool.com";
+
+    subHeading.innerHTML = "The only website dedicated to advanced education of the worlds BBQ cooks through home-study!";
+    
+    blurbDiv.className = "container";
+    
+    blurbDiv.id = "landing-blurb";
+    
+    blurbP.className = "blurb";
+    
+    blurbP.innerHTML = formatTextArray(json["landing"]["blurb"]);
+    
+    blurbDiv.appendChild(blurbP);
+    
+    text.appendChild(blurbDiv);
+}
+
+function renderAbout() 
+{
+    let heading = document.getElementsByClassName("heading")[0];
+    let subHeading = document.getElementsByClassName("sub")[0];
+    let text = document.getElementById("text");
+
+    heading.innerHTML = "";
+    subHeading.innerHTML = "";
+    text.innerHTML = "";
+
+    heading.innerHTML = "About Us"
+    
+    let whoDiv = document.createElement("div");
+    let whoP = document.createElement("p");
+
+    whoDiv.className = "container";
+    whoDiv.appendChild(whoP);
+    whoP.innerHTML = formatTextArray(json["about"]["who"]["instructor"]);
+    text.appendChild(whoDiv);
+}
+
+function renderContact() 
+{
+    let heading = document.getElementsByClassName("heading")[0];
+    let subHeading = document.getElementsByClassName("sub")[0];
+    let text = document.getElementById("text");
+
+    heading.innerHTML = "";
+    subHeading.innerHTML = "";
+    text.innerHTML = "";
+}
+
+function formatTextArray(toBeFormatted) {
+    let body = "";
+    toBeFormatted.forEach(string => {
+        body = body + "<br>" + string;
+    });
+    
+    return body;
+}
+
+StartUp();
